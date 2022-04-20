adbbcc584ead5965653acd4b36724f0183a4c00d
Your Name <you@example.com>
2022-04-20 17:05:03 +0000

    commit

--- sandwich/js/init.js
+++ sandwich/js/init.js
@@ -1,4 +1,4 @@
-import * as lateJs from 'https://cdn.jsdelivr.net/gh/00-59/late.js-68-00-59@1.1.0/dist/late.min.js';
+import * as lateJs from 'https://cdn.jsdelivr.net/gh/00-59/late.js-68-00-59@1.2.0/dist/late.min.js';
 
 (function($){
   $(function(){
@@ -9,21 +9,30 @@ import * as lateJs from 'https://cdn.jsdelivr.net/gh/00-59/late.js-68-00-59@1.1.
 })(jQuery); // end of jQuery name space
 
 
-var nodeList;
-nodeList = document.querySelectorAll("#page-footer");
-lateJs.addClassToNodes("purple", nodeList);
-nodeList = document.querySelectorAll("#title-text");
-lateJs.addClassToNodes("orange-text", nodeList);
-nodeList = document.querySelectorAll("#navbar");
-lateJs.addClassToNodes("light-green", nodeList);
-nodeList = document.querySelectorAll(".icon-text");
-lateJs.addClassToNodes("light-green-text", nodeList);
-nodeList = document.querySelectorAll(".lateButton");
-lateJs.addClassToNodes("btn-large", nodeList);
-lateJs.addClassToNodes("waves-effect", nodeList);
-lateJs.addClassToNodes("waves-light", nodeList);
-lateJs.addClassToNodes("purple", nodeList);
+lateJs.load();
 
 
-lateJs.loadMicrolightCode(microlight);
+function replaceClasses(){
+  var nodeList;
+  nodeList = document.querySelectorAll("#page-footer");
+  lateJs.addClassToNodes("purple", nodeList);
+  nodeList = document.querySelectorAll("#title-text");
+  lateJs.addClassToNodes("orange-text", nodeList);
+  nodeList = document.querySelectorAll("#navbar");
+  lateJs.addClassToNodes("light-green", nodeList);
+  nodeList = document.querySelectorAll(".icon-text");
+  lateJs.addClassToNodes("light-green-text", nodeList);
+  nodeList = document.querySelectorAll(".lateButton");
+  lateJs.addClassToNodes("btn-large", nodeList);
+  lateJs.addClassToNodes("waves-effect", nodeList);
+  lateJs.addClassToNodes("waves-light", nodeList);
+  lateJs.addClassToNodes("purple", nodeList);
+
+
+  lateJs.loadMicrolightCode(microlight);
+}
+
+setTimeout(function(){
+	replaceClasses()
+  }, 500);
 
