e21a126607fcdbf72f9c4f96132bc8bb4cc2a7a5
Your Name <you@example.com>
2022-08-16 03:54:22 +0000

    commit

---
+++ 1/js/init.js
@@ -0,0 +1,37 @@
+import * as smaloa from 'https://cdn.jsdelivr.net/gh/00-59/smaloa-68-00-59@4.2.2/smaloa.js';
+smaloa.CONFIG['components root']='./';
+
+import * as minilight from 'https://cdn.jsdelivr.net/gh/00-59/plugins-smaloa-68-00-59@4.1.2.1/plugins/minilight/minilight.js';
+
+var replaceClasses = function (){
+	var nodeList;
+	smaloa.addClassToQNodes("purple", "#page-footer");
+	smaloa.addClassToQNodes("orange-text", "#title-text");
+	smaloa.addClassToQNodes("light-green", "#navbar");
+	smaloa.addClassToQNodes("light-green-text", ".icon-text");
+	smaloa.addClassToQNodes("btn-large", ".lateButton");
+	smaloa.addClassToQNodes("waves-effect",  ".lateButton");
+	smaloa.addClassToQNodes("waves-light",  ".lateButton");
+	smaloa.addClassToQNodes("purple",  ".lateButton");
+
+
+	minilight.loadMicrolightCode(smaloa);
+}
+
+smaloa.CONFIG['phase 3'] = replaceClasses;
+smaloa.load();
+
+
+
+
+// load jquery
+// related with materializeCSS
+// not related with smaloa
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav(); // display sidenav in portrait view
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
